59b43121a7e3acfc0f582a9d9a509e5fe0b2d77a
GoncaloRod <rodriguesgonalo604@gmail.com>
2017-02-20 22:10:51 +0000

    Prevent the user from writing letters in a TextBox in C#

---
+++ TextBoxKeyPress/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TextBoxKeyPress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_ApenasNumeros = new System.Windows.Forms.Label();
+            this.txt_Texto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_ApenasNumeros
+            // 
+            this.lbl_ApenasNumeros.AutoSize = true;
+            this.lbl_ApenasNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ApenasNumeros.Location = new System.Drawing.Point(12, 9);
+            this.lbl_ApenasNumeros.Name = "lbl_ApenasNumeros";
+            this.lbl_ApenasNumeros.Size = new System.Drawing.Size(123, 18);
+            this.lbl_ApenasNumeros.TabIndex = 0;
+            this.lbl_ApenasNumeros.Text = "Apenas Numeros";
+            // 
+            // txt_Texto
+            // 
+            this.txt_Texto.Location = new System.Drawing.Point(15, 40);
+            this.txt_Texto.Name = "txt_Texto";
+            this.txt_Texto.Size = new System.Drawing.Size(213, 20);
+            this.txt_Texto.TabIndex = 1;
+            this.txt_Texto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Texto_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 78);
+            this.Controls.Add(this.txt_Texto);
+            this.Controls.Add(this.lbl_ApenasNumeros);
+            this.Name = "Form1";
+            this.Text = "KeyPress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_ApenasNumeros;
+        private System.Windows.Forms.TextBox txt_Texto;
+    }
+}
+
---
+++ TextBoxKeyPress/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxKeyPress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_Texto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Dados
+            long n;
+            TextBox txt = (TextBox)sender;
+            if ((!long.TryParse(e.KeyChar.ToString(), out n)) && (!(e.KeyChar == Convert.ToChar(Keys.Back))))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
